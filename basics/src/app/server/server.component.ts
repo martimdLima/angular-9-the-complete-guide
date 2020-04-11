864eabb60a1120676691198228c10f034ae50801
Martim <martim.d.lima@protonmail.com>
2020-04-11 23:18:38 +0100

    Angular 9 - Basics - Applying CSS Classes Dynamically with ngClass

--- basics/src/app/server/server.component.ts
+++ basics/src/app/server/server.component.ts
@@ -3,6 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [`
+  .online{
+    color: white;
+  }`]
 })
 export class ServerComponent {
 
