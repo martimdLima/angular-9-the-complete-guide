609b5187a640b6731bb504473a7de8a34fdc7d9b
Martim <martim.d.lima@protonmail.com>
2020-04-11 16:32:35 +0100

    Angular 9 - Basics - Property Binding

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [`h3 {
+  styleUrls: ['./app.component.css']
+  /*styles: [`h3 {
     color: dodgerblue;
-  }`]
+  }`]*/
 })
 export class AppComponent {
   title = 'basics';
