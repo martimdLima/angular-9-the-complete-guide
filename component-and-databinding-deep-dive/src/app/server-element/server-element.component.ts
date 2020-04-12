ad90e72bc192e9033c119a6fb18f40377c1cedb7
Martim <martim.d.lima@protonmail.com>
2020-04-12 23:07:18 +0100

    Angular 9 - Components & Databinding Deep Dive - Understanding View Encapsulation

--- component-and-databinding-deep-dive/src/app/server-element/server-element.component.ts
+++ component-and-databinding-deep-dive/src/app/server-element/server-element.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styles: [
-  ]
+  styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
 
