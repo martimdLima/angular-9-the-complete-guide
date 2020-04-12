cc0cd8e102a4b09f395e2bdda01908861d9e9d55
Martim <martim.d.lima@protonmail.com>
2020-04-12 16:39:03 +0100

    Angular 9 - Course Project - Adding Content to the Recipes Components

--- angular9-the-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular9-the-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes : Recipe[] = [
+    new Recipe("Test Recipe 1","Test Recipe 1","https://i.ytimg.com/vi/BFrkRFgHLVk/hqdefault.jpg"),
+    new Recipe("Test Recipe 2","Test Recipe 2","https://thedolcediet.com/wp-content/uploads/2016/01/meatball-1.jpg"),
+    new Recipe("Test Recipe 3","Test Recipe 3","https://img1.mashed.com/img/gallery/fast-food-hamburgers-ranked-worst-to-best/intro-1540401194.jpg"),
+    new Recipe("Test Recipe 4","Test Recipe 4","https://media.salon.com/2015/04/shutterstock_84904876.jpg"),
+  ];
 
   constructor() { }
 
