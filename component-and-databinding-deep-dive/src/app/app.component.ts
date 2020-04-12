34d226b7804adf90e45fa19d666f7455fb77a945
Martim <martim.d.lima@protonmail.com>
2020-04-12 20:11:06 +0100

    Angular 9 - Components & Databinding Deep Dive - Binding to Custom Properties

--- component-and-databinding-deep-dive/src/app/app.component.ts
+++ component-and-databinding-deep-dive/src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-
-  serverElements = [];
+  serverElements = [{type: 'server', name: 'TestServer1', content: 'Test Content'},
+                    {type: 'server', name: 'TestServer2', content: 'Test Content'},
+                    {type: 'blueprint', name: 'TestServer3', content: 'Test Content'},
+                    {type: 'blueprint', name: 'TestServer4', content: 'Test Content'}];
 }
