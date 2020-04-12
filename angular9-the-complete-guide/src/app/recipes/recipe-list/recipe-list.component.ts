e55367364f487e611d076827e2a062c9f4fa12f7
Martim <martim.d.lima@protonmail.com>
2020-04-12 16:55:22 +0100

    Angular 9 - Course Project - Displaying Recipe Details

--- angular9-the-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular9-the-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,10 +9,10 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes : Recipe[] = [
-    new Recipe("Test Recipe 1","Test Recipe 1","https://i.ytimg.com/vi/BFrkRFgHLVk/hqdefault.jpg"),
-    new Recipe("Test Recipe 2","Test Recipe 2","https://thedolcediet.com/wp-content/uploads/2016/01/meatball-1.jpg"),
-    new Recipe("Test Recipe 3","Test Recipe 3","https://img1.mashed.com/img/gallery/fast-food-hamburgers-ranked-worst-to-best/intro-1540401194.jpg"),
-    new Recipe("Test Recipe 4","Test Recipe 4","https://media.salon.com/2015/04/shutterstock_84904876.jpg"),
+    new Recipe("Lasagna","Test Recipe 1","https://i.ytimg.com/vi/BFrkRFgHLVk/hqdefault.jpg"),
+    new Recipe("Meatballs","Test Recipe 2","https://thedolcediet.com/wp-content/uploads/2016/01/meatball-1.jpg"),
+    new Recipe("Hamburger","Test Recipe 3","https://img1.mashed.com/img/gallery/fast-food-hamburgers-ranked-worst-to-best/intro-1540401194.jpg"),
+    new Recipe("Pizza","Test Recipe 4","https://media.salon.com/2015/04/shutterstock_84904876.jpg"),
   ];
 
   constructor() { }
