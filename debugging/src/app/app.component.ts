b56f32d789436b60c499e5172ebcc3b5e3d44961
Martim <martim.d.lima@protonmail.com>
2020-04-12 17:50:35 +0100

    Angular 9 - Debugging  - Understanding Angular Error Messages

---
+++ debugging/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  // without defining the servers array, we get an error
+  // 'Cannot read property 'push' of undefined'
+  //   servers;
+
+  servers: string[] = [];
+
+  onAddServer() {
+    this.servers.push('Another Server');
+  }
+
+  onRemoveServer(id: number) {
+    const position = id + 1;
+    this.servers.splice(position, 1);
+  }
+}
