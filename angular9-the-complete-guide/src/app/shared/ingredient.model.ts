44f1cbbc2349290c1cc0408bd9c2faad1e999e5d
Martim <martim.d.lima@protonmail.com>
2020-04-12 17:07:44 +0100

    Angular 9 - Course Project  - Creating an "Ingredient" Model

---
+++ angular9-the-complete-guide/src/app/shared/ingredient.model.ts
@@ -0,0 +1,18 @@
+export class Ingredient {
+
+    /*
+   * Constructors can be built two ways in Angular
+     public name: string;
+     public amount: number;
+
+
+     constructor(name: string, amount: number) {
+      this.name = name;
+      this.amount = amount;
+  }
+  */
+
+  constructor(public name: string, public amount: number) {
+  }
+
+}
