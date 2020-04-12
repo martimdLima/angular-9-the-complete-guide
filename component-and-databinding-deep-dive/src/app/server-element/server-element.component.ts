a2dbc19042a48e3746052c6e2ac2dcb5f7df0c29
Martim <martim.d.lima@protonmail.com>
2020-04-12 19:34:07 +0100

    Angular 9 - Components & Databinding Deep Dive - Splitting Apps into Components

---
+++ component-and-databinding-deep-dive/src/app/server-element/server-element.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styles: [
+  ]
+})
+export class ServerElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
