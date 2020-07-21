13d78be26904aca9a6b47c0787615a4650738314
Martim <martim.d.lima@protonmail.com>
2020-07-21 20:12:13 +0100

    Angular 9 - Components & Databinding Deep Dive - Seeing LifeCycle Hooks in Action

--- component-and-databinding-deep-dive/src/app/server-element/server-element.component.ts
+++ component-and-databinding-deep-dive/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, SimpleChanges, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -7,13 +7,53 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.Emulated //None, Native, Emulated ;
 })
 
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
 
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor called');
   }
 
+  // A lifecycle hook that is called when any data-bound property of a directive changes
+  ngOnChanges(changes: SimpleChanges ) {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+
+  // A lifecycle hook that is called after Angular has initialized all data-bound properties of a directive.
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+  }
+
+  //The default change-detection algorithm looks for differences by comparing bound-property values by reference across change detection runs.
+  //You can use this hook to check for and respond to changes by some other means.
+  ngDoCheck() {
+    console.log('ngDoCheck called');
+  }
+
+  // A lifecycle hook that is called after Angular has fully initialized all content of a directive.
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called');
+  }
+
+  // A lifecycle hook that is called after the default change detector has completed checking all content of a directive.
+  ngAfterContentChecked() {
+    console.log('ngAfterContentCheck called');
+  }
+
+  // A lifecycle hook that is called after Angular has fully initialized a component's view.
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+  }
+
+  // A lifecycle hook that is called after the default change detector has completed checking a component's view for changes.
+  ngAfterViewChecked() {
+    console.log('ngAfterViewCheck called');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called');
+  }
 }
