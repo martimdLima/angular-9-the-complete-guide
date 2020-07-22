4883d7bed3770589091e163e34e97bf0b6bdb393
Martim <martim.d.lima@protonmail.com>
2020-07-22 14:57:32 +0100

    Angular 9 - Course Project - Components & Databinding - Adding Navigation with Event Binding and ngIf

--- course_project/src/app/app.component.ts
+++ course_project/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular9-the-complete-guide';
+
+  loadedFeature = 'recipe';
+
+  onNavigate(featureSelected: string) {
+    this.loadedFeature = featureSelected;
+  }
 }
