3edbd0ef793f74b9e863b750d32a62dff13639d2
mdLima0 <martim.d.lima@protonmail.com>
2020-07-23 20:38:12 +0100

    Angular 9 - Setting up and Loading Routes - An Important Note about Route Observables

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
+  // Although Angular already destroys the observables linked to a given component when it's not used anymore, that is not the case with custom observables,
+  // hence the need to destroy them when the component is not used anymore.
+
   user: { id: number; name: string };
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -17,9 +22,13 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['name'],
     };
 
-    this.route.params.subscribe((params: Params) => {
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
       this.user.id = params['id'];
       this.user.name = params['name'];
     });
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 }
