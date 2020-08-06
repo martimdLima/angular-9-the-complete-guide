6bbe540bae6840c0570d5f6c29a9e20d2203b29d
mdLima0 <martim.d.lima@protonmail.com>
2020-08-06 23:15:28 +0100

    Angular 9 - Changing Pages with Routing - Controlling Navigation with canDeactivate

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
+import { Observable } from 'rxjs';
+import { toBase64String } from '@angular/compiler/src/output/source_map';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css'],
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: { id: number; name: string; status: string };
   serverName = '';
   serverStatus = '';
@@ -29,8 +32,8 @@ export class EditServerComponent implements OnInit {
       this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
     });
     this.route.fragment.subscribe();
-
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
@@ -42,5 +45,16 @@ export class EditServerComponent implements OnInit {
     });
     this.changesSaved = true;
     this.router.navigate(['../'], {relativeTo: this.route});
+  }
+
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    if(!this.allowEdit) {
+      return true;
+    }
+    if((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changesSaved) {
+     return confirm('Do you want to discard the changes?');
+    } else {
+      return true;
+    }
   }
 }
