81fefdd1b610c545ad087892d63c74eec2ea58fd
Martim Lima <martim.d.lima@protonmail.com>
2020-08-16 20:28:53 +0100

    Angular 9 - Understanding Observables - Understanding Errors & Completion in Observables

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -21,16 +21,37 @@ export class HomeComponent implements OnInit, OnDestroy {
     const customIntervalObservable = Observable.create((observer) => {
       let count = 0;
       setInterval(() => {
+        // A handler for each delivered value. Called zero or more times after execution starts. - Required
         observer.next(count);
+
+        // A handler for the execution-complete notification.
+        // Delayed values can continue to be delivered to the next handler after execution is complete. - Optional
+        if (count == 2) {
+          observer.complete();
+        }
+
+        //  A handler for an error notification.
+        // An error halts execution of the observable instance. - Optional
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'));
+        }
+
         count++;
-        //observer.error();
-        //observer.complete()
       }, 1000);
     });
 
-    this.observableSubscription = customIntervalObservable.subscribe((data) => {
-      console.log(data);
-    });
+    this.observableSubscription = customIntervalObservable.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log('Completed!');
+      }
+    );
   }
 
   ngOnDestroy() {
