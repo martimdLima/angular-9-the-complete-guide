72ef5c28df6c3859238bd1bc1d216c4dc0b3408f
Martim Lima <martim.d.lima@protonmail.com>
2020-08-16 20:03:48 +0100

    Angular 9 - Understanding Observables - Undestanding Observables lifecycle

---
+++ observables-start/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { OnInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ observables-start/src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  private observableSubscription: Subscription;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.observableSubscription = interval(1000).subscribe((count) => {
+      console.log(count);
+    });
+  }
+
+  ngOnDestroy() {
+    this.observableSubscription.unsubscribe();
+  }
+}
