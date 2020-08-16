a8129ea885f7248da16b4837f4d2e91f4060a3c6
Martim Lima <martim.d.lima@protonmail.com>
2020-08-16 13:13:16 +0100

    Angular 9 - Course Project - Adding Child Routing

--- course_project/src/app/app.module.ts
+++ course_project/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-ed
 import { DropDownDirective } from "./shared/dropdown.directive";
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { AppRoutingModule } from "./app-routing.module";
+import { RecipeStartComponent } from "./recipes/recipes-start/recipe-start.component";
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { AppRoutingModule } from "./app-routing.module";
     ShoppingListComponent,
     ShoppingEditComponent,
     DropDownDirective,
+    RecipeStartComponent,
   ],
   imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
   providers: [ShoppingListService],
