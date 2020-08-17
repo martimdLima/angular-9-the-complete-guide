37fb991302ee69d0e6e64669577a618b2b42abef
Martim Lima <martim.d.lima@protonmail.com>
2020-08-17 14:57:52 +0100

    Angular 9 - Handling Forms in Angular Apps - TD: Resetting Forms

--- forms-start/src/app/app.component.ts
+++ forms-start/src/app/app.component.ts
@@ -66,6 +66,10 @@ export class AppComponent {
     this.user.secretQuestion = this.signupForm.value.secret;
     this.user.answer = this.signupForm.value.answer;
     this.user.gender = this.signupForm.value.gender;
+
+    // this can also be accomplished by passing the same object as in setValue() to reset(), which will then reset the form to specific values
+
+    this.signupForm.reset();
   }
 }
 
