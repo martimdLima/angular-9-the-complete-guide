53026b788c6cce95c794182483b431da608ee445
Martim Lima <martim.d.lima@protonmail.com>
2020-08-17 14:16:39 +0100

    Angular 9 - Handling Forms in Angular Apps -  TD: Setting and Patching Form Values

--- forms-start/src/app/app.component.ts
+++ forms-start/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  //@ViewChild('f') signupForm: NgForm;
+  @ViewChild('f') signupForm: NgForm;
 
   defaultQuestion = 'teacher';
   answer = '';
@@ -15,6 +15,30 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    /*
+    // We use the SetValue to update the FormControl , FormGroup or FormArray. 
+    // When we use it to update the FormGroup or FormArray the SetValue requires that the object must match the structure of the FormGroup or FormArray exactly. 
+    // Otherwise, it will result in an error.
+
+    this.signupForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: ''
+      },
+      secret: 'pet',
+      questionAnswer:'',
+      gender: 'male'
+    });
+    */
+
+    // The PatchValue is used to update only a subset of the elements of the FormGroup or FormArray.
+    // It will only update the matching objects and ignores the rest.
+
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
   }
 
   onSubmit(form: NgForm) {
