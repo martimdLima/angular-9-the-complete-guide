96845a9a340fb4432c26e38f420b5cff9a355bc0
Martim Lima <martim.d.lima@protonmail.com>
2020-08-17 23:46:39 +0100

    Angular 9 - Handling Forms in Angular Apps - Reactive: Grouping Controls

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -7,7 +7,6 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  
   genders = ['male', 'female'];
   signupForm: FormGroup;
 
