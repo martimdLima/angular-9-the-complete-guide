ca35fa964a5e0b5b9ce87402b013d7f2a04e9dc0
Martim Lima <martim.d.lima@protonmail.com>
2020-08-19 20:40:51 +0100

    Angular 9 - Making Http Requests - Using RxJS Operators to Transform Response Data

--- http-start/src/app/app.component.ts
+++ http-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -45,6 +46,18 @@ export class AppComponent implements OnInit {
   private fetchPosts() {
     this.http
       .get('https://ng-complete-guide-c8703.firebaseio.com/posts.json')
+      .pipe(
+        map((responseData) => {
+          const postsArray = [];
+
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )
       .subscribe((posts) => {
         console.log(posts);
       });
