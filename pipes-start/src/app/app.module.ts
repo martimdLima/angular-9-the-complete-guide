7136793704bc4eb18805418a38bc1c3169633a9b
Martim Lima <martim.d.lima@protonmail.com>
2020-08-19 17:05:23 +0100

    Angular 9 - Using Pipes to Transform Output - Creating a Custom Pipe

--- pipes-start/src/app/app.module.ts
+++ pipes-start/src/app/app.module.ts
@@ -5,9 +5,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { ShortenPipe } from './shorten.pipe';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShortenPipe
   ],
   imports: [
     BrowserModule,
