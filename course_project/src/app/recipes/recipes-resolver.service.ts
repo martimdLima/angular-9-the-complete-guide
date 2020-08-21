db6079ef2f47aa88bb73ecf54cc0839c3f3cdb1d
Martim Lima <martim.d.lima@protonmail.com>
2020-08-21 15:56:41 +0100

    Angular 9 - Course Project - Http - Fixing a Bug with the Resolver

--- course_project/src/app/recipes/recipes-resolver.service.ts
+++ course_project/src/app/recipes/recipes-resolver.service.ts
@@ -2,12 +2,23 @@ import { Injectable } from "@angular/core";
 import { Resolve, ActivatedRoute, RouterStateSnapshot } from "@angular/router";
 import { Recipe } from "./recipe.model";
 import { DataStorageService } from "../shared/data-storage.service";
+import { RecipeService } from './recipe.service';
 
 @Injectable({ providedIn: "root" })
 export class RecipesResolverService implements Resolve<Recipe[]> {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(private dataStorageService: DataStorageService,
+              private recipeService: RecipeService) {}
+
+
 
   resolve(route: ActivatedRoute, state: RouterStateSnapshot) {
-    return this.dataStorageService.fetchRecipes();
+
+    const recipes = this.recipeService.getRecipeList();
+
+    if(recipes.length === 0) {
+        return this.dataStorageService.fetchRecipes();
+    } else {
+        return recipes;
+    }
   }
 }
