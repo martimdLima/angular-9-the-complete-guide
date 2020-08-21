39815715afd820644d2899b2eff35b273b7a6b27
Martim Lima <martim.d.lima@protonmail.com>
2020-08-21 11:38:40 +0100

    Angular 9 - Making Http Requests - Adding Query Params

--- http-start/src/app/posts.service.ts
+++ http-start/src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Post } from './post.model';
 import { map, catchError } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
@@ -29,11 +29,14 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
     return this.http
       .get<{ [key: string]: Post }>(
-        'https://ng-complete-guide-c8703.firebaseio.com/posts.json', 
+        'https://ng-complete-guide-c8703.firebaseio.com/posts.json',
         {
-          headers: new HttpHeaders({'Custom-Header' : 'Hello'})
+          headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+          params: new HttpParams().set('print', 'pretty'),
         }
       )
       .pipe(
