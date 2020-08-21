060ed6693c1b74b6f4944fcf33d5dcfec75c6bcf
Martim Lima <martim.d.lima@protonmail.com>
2020-08-21 17:45:11 +0100

    Angular 9 - Course Project - Authentication & Route Protection in Angular
    Sending the Signup Request

--- course_project/src/app/auth/auth.component.ts
+++ course_project/src/app/auth/auth.component.ts
@@ -1,22 +1,39 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component({
   selector: "app-auth",
   templateUrl: "./auth.component.html",
 })
 export class AuthComponent {
+  constructor(private authService: AuthService) {}
 
-    constructor() {}
+  isLoginMode = true;
 
-    isLoginMode = true;
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 
-    onSwitchMode() {
-        this.isLoginMode = !this.isLoginMode;
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
     }
 
-    onSubmit(form: NgForm) {
-        console.log(form.value);
-        form.reset();
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginMode) {
+    } else {
+      this.authService.signupNewUser(email, password).subscribe(
+        (responseData) => {
+          console.log(responseData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     }
+    form.reset();
+  }
 }
