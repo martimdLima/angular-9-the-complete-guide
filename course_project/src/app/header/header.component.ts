b47fe93290bfcb2c0fbc7e887b4e0f05fdc01eda
Martim Lima <martim.d.lima@protonmail.com>
2020-08-21 13:43:35 +0100

    Angular 9 - Course Project - Http - Storing Recipes

--- course_project/src/app/header/header.component.ts
+++ course_project/src/app/header/header.component.ts
@@ -1,9 +1,15 @@
 import { Component } from "@angular/core";
+import { DataStorageService } from "../shared/data-storage.service";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
 })
 export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {}
   collapsed: boolean = true;
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
 }
