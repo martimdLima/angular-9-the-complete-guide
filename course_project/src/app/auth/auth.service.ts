7be5d3f0dd7688e6e6b46c8f01ea7e7c31754d89
Martim Lima <martim.d.lima@protonmail.com>
2020-08-26 00:56:20 +0100

    Angular 9 - Course Project - Authentication & Route Protection in Angular
    Creating & Storing the User Data

--- course_project/src/app/auth/auth.service.ts
+++ course_project/src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { throwError, Subject } from "rxjs";
+import { User } from './user.model';
 
 export interface AuthResponseData {
   idToken: string;
@@ -14,6 +15,9 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
+
+  user = new Subject<User>();
+
   constructor(private httpClient: HttpClient) {}
 
   signupNewUser(email: string, password: string) {
@@ -27,7 +31,9 @@ export class AuthService {
         }
       )
       .pipe(
-        catchError(this.handleError)
+        catchError(this.handleError), tap((responseData) => {
+          this.handleAuthentication(responseData.email, responseData.localId, responseData.idToken, +responseData.expiresIn);
+        })
       );
   }
 
@@ -40,7 +46,15 @@ export class AuthService {
         returnSecureToken: true,
       }
     )
-    .pipe(catchError(this.handleError));
+    .pipe(catchError(this.handleError), tap((responseData) => {
+      this.handleAuthentication(responseData.email, responseData.localId, responseData.idToken, +responseData.expiresIn);
+    }));
+  }
+
+  private handleAuthentication(email: string, userId: string,  token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
   }
 
   private handleError(errorResponse: HttpErrorResponse) {
