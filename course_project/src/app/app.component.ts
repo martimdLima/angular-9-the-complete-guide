eff7e756d080aa4cde663e6b9d7d1a7c0509487a
Martim Lima <martim.d.lima@protonmail.com>
2020-08-26 15:51:17 +0100

    Angular 9 - Course Project - Authentication & Route Protection in Angular
    - Useful Resources

--- course_project/src/app/app.component.ts
+++ course_project/src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements OnInit {
     Useful Resources
       https://firebase.google.com/docs/reference/rest/auth/
       https://loading.io/css
+      https://jwt.io/
   */
 
   ngOnInit() {
