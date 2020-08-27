b09a2589937f4ca9366f59217f74e4845ff05b76
Martim Lima <martim.d.lima@protonmail.com>
2020-08-27 12:52:22 +0100

    Angular 9 - Course Project - Working with NgRx
    - Adding Logic to the Reducer

--- course_project/src/app/shopping-list/shopping-list.reducer.ts
+++ course_project/src/app/shopping-list/shopping-list.reducer.ts
@@ -1,18 +1,26 @@
-import { Ingredient } from '../shared/ingredient.model';
+import { Ingredient } from "../shared/ingredient.model";
+import { Action } from "@ngrx/store";
 
 const initialState = {
-    ingredientList: [
-        new Ingredient("Tomatoes", 10),
-        new Ingredient("Onions", 5),
-        new Ingredient("Basil", 2),
-        new Ingredient("Bell Peppers", 4),
-        new Ingredient("lettuce", 10),
-        new Ingredient("Potatoes", 5),
-        new Ingredient("Pasta", 5),
-        new Ingredient("Cheese", 4),
-        new Ingredient("Meat", 10),
-      ]
+  ingredientList: [
+    new Ingredient("Tomatoes", 10),
+    new Ingredient("Onions", 5),
+    new Ingredient("Basil", 2),
+    new Ingredient("Bell Peppers", 4),
+    new Ingredient("lettuce", 10),
+    new Ingredient("Potatoes", 5),
+    new Ingredient("Pasta", 5),
+    new Ingredient("Cheese", 4),
+    new Ingredient("Meat", 10),
+  ],
 };
 
-export function ShoppingListReducer(state = initialState, action) {
-} 
+export function ShoppingListReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case "ADD_INGREDIENT":
+      return {
+        ...state,
+        ingredientList: [...state.ingredientList, action],
+      };
+  }
+}
