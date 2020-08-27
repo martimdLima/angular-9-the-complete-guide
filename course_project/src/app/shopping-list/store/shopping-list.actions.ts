0b6cd1dba40ac47d9ac3835441d880d66a6ed669
Martim Lima <martim.d.lima@protonmail.com>
2020-08-27 14:12:03 +0100

    Angular 9 - Course Project - Working with NgRx
    - Preparing Update & Delete Actions

--- course_project/src/app/shopping-list/store/shopping-list.actions.ts
+++ course_project/src/app/shopping-list/store/shopping-list.actions.ts
@@ -3,8 +3,10 @@ import { Ingredient } from "../../shared/ingredient.model";
 
 export const ADD_INGREDIENT = "ADD_INGREDIENT";
 export const ADD_INGREDIENTS = "ADD_INGREDIENTS";
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 
-export type ShoppingListActions = AddIngredient | AddIngredients;
+export type ShoppingListActions = AddIngredient | AddIngredients | UpdateIngredient | DeleteIngredient;
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
@@ -16,4 +18,16 @@ export class AddIngredients implements Action {
   readonly type = ADD_INGREDIENTS;
 
   constructor(public payload: Ingredient[]) {}
+}
+
+export class UpdateIngredient implements Action {
+    readonly type = UPDATE_INGREDIENT;
+    
+    constructor(public payload: {index: number, ingredient: Ingredient}) {}
+}
+
+export class DeleteIngredient implements Action {
+    readonly type = DELETE_INGREDIENT;
+
+    constructor(public payload: {index: number}) {}
 }
