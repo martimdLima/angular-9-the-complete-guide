a814501fde5b11267f885fde95d70cb23dfec037
Martim Lima <martim.d.lima@protonmail.com>
2020-08-28 23:53:43 +0100

    Angular 9 - Course Project - Working with NgRx
    - Useful Resources

--- course_project/src/app/app.component.ts
+++ course_project/src/app/app.component.ts
@@ -25,9 +25,15 @@ export class AppComponent implements OnInit {
       https://angular.io/guide/ngmodule-faq
       Server Routing vs Browser Routing 
       https://academind.com/learn/angular/angular-q-a/#how-to-fix-broken-routes-after-deployment
+      https://ngrx.io/docs
 
       Deployed App
       https://ng-course-recipe-book-4d4d5.web.app/recipes
+  */
+
+  /* TODO
+    * Update the NgRx syntax used in this course, for the new updated syntax provided in this link:
+      https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/14466642#questions/7350498
   */
 
   ngOnInit() {
