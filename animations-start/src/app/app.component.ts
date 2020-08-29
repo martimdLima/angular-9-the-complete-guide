c75ca7caf32480432500a544cb9eee8327b2291e
Martim Lima <martim.d.lima@protonmail.com>
2020-08-29 23:20:43 +0100

    Angular 9 - Angular animations
    - Using Animation Callbacks

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -167,4 +167,12 @@ export class AppComponent {
   onShrink() {
     this.wildState = 'shrunken';
   }
+
+  animationStarted(event){
+    console.log(event);
+  }
+
+  animationEnded(event){
+    console.log(event);
+  }
 }
