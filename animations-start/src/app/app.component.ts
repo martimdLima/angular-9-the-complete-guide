609e0f0751bab2ddc1b351bd8da5152cae232e70
Martim Lima <martim.d.lima@protonmail.com>
2020-08-29 17:39:27 +0100

    Angular 9 - Angular animations
    - Transitions

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style } from '@angular/animations';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -20,6 +20,8 @@ import { trigger, state, style } from '@angular/animations';
           transform: 'translateX(100px)',
         })
       ),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
     ]),
   ],
 })
@@ -40,6 +42,6 @@ export class AppComponent {
   }
 
   onShrink() {
-    
+
   }
 }
