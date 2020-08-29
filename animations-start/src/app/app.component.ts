7f17ac27312b46f4a17b388b62ab2e353095055d
Martim Lima <martim.d.lima@protonmail.com>
2020-08-29 23:05:25 +0100

    Angular 9 - Angular animations
    - Using Keyframes for Animations

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -5,6 +5,7 @@ import {
   style,
   transition,
   animate,
+  keyframes,
 } from '@angular/animations';
 
 @Component({
@@ -77,17 +78,60 @@ import {
       transition('void => *', [
         style({
           opacity: 0,
-          transform: 'translateX(-100px)'
+          transform: 'translateX(-100px)',
         }),
-        animate(300)
+        animate(300),
       ]),
       transition('* => void', [
+        animate(
+          300,
+          style({
+            transform: 'translateX(100px)',
+            opacity: 0,
+          })
+        ),
+      ]),
+    ]),
+    trigger('list2', [
+      state(
+        'in',
         style({
-        }),
-        animate(300, style({
-          transform: 'translateX(100px)',
-          opacity: 0
-        }))
+          opacity: 1,
+          transform: 'translateX(0)',
+        })
+      ),
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-25px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
+      ]),
+      transition('* => void', [
+        animate(
+          300,
+          style({
+            transform: 'translateX(100px)',
+            opacity: 0,
+          })
+        ),
       ]),
     ])
   ],
