56f693685ed710fef31f98da32e5628aaec39bd7
Martim Lima <martim.d.lima@protonmail.com>
2020-08-30 20:33:41 +0100

    Angular 9 - Adding Offline Capabilities with Service Workers
    Adding Service Workers

--- service-workers-start/src/app/app.component.ts
+++ service-workers-start/src/app/app.component.ts
@@ -9,6 +9,16 @@ import { Post } from './post.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+  /*
+    https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/10617918#questions/11379350
+    https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/10617918#questions/11421670
+
+
+    // HTTP Server command
+      http-server-spa service-workers-start
+  */
+
   posts: Post[] = [];
 
   constructor(private http: HttpClient) {}
--- service-workers-start/src/app/app.module.ts
+++ service-workers-start/src/app/app.module.ts
@@ -4,10 +4,15 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { SwUpdateComponent } from './sw-update/sw-update.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
-  declarations: [AppComponent, PostComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, PostComponent, SwUpdateComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
